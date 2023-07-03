9b816a18ae7714e7bc23746182e0fb5cec324f99
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-03 10:15:17 +0300

    work almost done

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+function cats(name){
+let cats= ["Milo", "Otis", "Garfield"];
+  cats.length=0
+}
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+  }
+
+function appendCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"];
+  
+    const newCats = [...cats, Broom];
+  
+    return newCats;
+}
+
+cats.length=0
+function prependCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"];
+  
+    const newCats = ['Arnold', ...cats];
+  
+    return newCats;
+  }
+
+  function removeLastCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+  
+    const newCats = cats.slice(0, cats.length - 1);
+  
+    return newCats;
+  }
+
+function removeFirstCat() {
+  const cats = ["Milo", "Otis", "Garfield"];
+
+  const newCats = cats.slice(1);
+
+  return newCats;
+}
+
